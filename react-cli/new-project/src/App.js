e350becbc8268fbe56016d269d6584202aefd73c
AayushHexbis <aayush.joshi.hexbis@gmail.com>
2021-09-29 12:28:36 +0530

    done axios,fetch,components,react-lifecycle

---
+++ react-cli/new-project/src/App.js
@@ -0,0 +1,44 @@
+import React from "react"
+import './CSS/app.css'
+import Form from './Components/Form'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import Axios from "./Components/Axios"
+import FetchGet from "./Components/FetchGet"
+import FetchPost from "./Components/FetchPost"
+import AxiosPost from "./Components/AxiosPost"
+import AxiosGet from "./Components/AxiosGet"
+import Callback from "./Components/Callback"
+import Toggle from "./Components/Toggle"
+import Bakery from "./Components/Bakery"
+
+
+const myFun = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+// this.myFun = this.myFun.bind(this)
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    {/* <Form /> */}
+    {/* <Axios /> */}
+    {/* <Footer /> */}
+    {/* <FetchGet /> */}
+    {/* <FetchPost/> */}
+    {/* <AxiosPost/> */}
+    {/* <AxiosGet/> */}
+    {/* <Callback pro={myFun}/> */}
+    <Bakery/>
+    {/* <Toggle /> */}
+    </>
+  );
+}
+
+export default App;
